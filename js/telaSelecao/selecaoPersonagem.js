6a90f88eb87f880c7c5d375e7743d36623906845
Keven <kevendicamargo@gmail.com>
2024-11-12 14:43:16 -0300

    refactor: Arquivo selecaoPersonagem.js
    Exportando funções para a index;
    Movendo a função exbirCard para o arquivo;
    Alterando a função escolherPersonagem para salvarPersonagem;
    Inserindo funções para retornar o personagem selecionado.

--- js/telaSelecao/selecaoPersonagem.js
+++ js/telaSelecao/selecaoPersonagem.js
@@ -3,7 +3,7 @@ let personagensSelecionados = {
     jogador2: null
 };
 
-function exibirJodador1() {
+export function exibirJodador1() {
     if (personagensSelecionados.jogador1 == null) {
         const personagens = document.querySelectorAll('.personagem');
 
@@ -31,37 +31,74 @@ function exibirJodador1() {
     }
 }
 
-function exibirJodador2() {
-    if (personagensSelecionados.jogador1 != null) {
-        const personagens = document.querySelectorAll('.personagem-2');
+export function exibirCardPersonagem1() {
+    const cards = document.querySelectorAll('.icon');
+    const cardElement = document.getElementById('card');
 
-        personagens.forEach((personagem) => {
+    cards.forEach((icon) => {
+        icon.addEventListener('mouseenter', () => {
+            const idSelecionado = icon.id;
 
-            personagem.addEventListener('mouseenter', () => {
+            cards.forEach((card) => {
+                card.classList.remove('selecionado');
+            });
 
-                const idSelecionado = personagem.attributes.id.value;
+            icon.classList.add('selecionado');
 
-                const personagemSelecionado = document.querySelector('.jogador-2-selecionado');
-                personagemSelecionado.classList.remove('jogador-2-selecionado');
+            cardElement.innerHTML = `<img class="card" src="img/imgPersona/${idSelecionado}.jpeg" alt="Personagem ${idSelecionado}">`;
+        });
 
-                personagem.classList.add('jogador-2-selecionado');
+        icon.addEventListener('mouseleave', () => {
 
-                const imagemJogador2 = document.getElementById('personagem-jogador-2');
-                imagemJogador2.src = `img/imgPersona/${idSelecionado}.png`;
+            cardElement.innerHTML = '';
 
-                const nomejogador2 = document.getElementById('nome-jogador-2');
-                const nomeSelecionado = personagem.getAttribute('data-name');
-
-                nomejogador2.innerHTML = nomeSelecionado;
-
-            })
-        })
-    }
-
+        });
+    });
 }
 
-function escolherPersonagem(jogador, personagem) {
+export function exibirJodador2() {
+        const personagens = document.querySelectorAll('.personagem-2');
+    
+            personagens.forEach((personagem) => {
+    
+                personagem.addEventListener('mouseenter', () => {
+    
+                const idSelecionado = personagem.attributes.id.value;
+    
+                const personagemSelecionado = document.querySelector('.jogador-2-selecionado');
+                personagemSelecionado.classList.remove('jogador-2-selecionado');
+    
+                personagem.classList.add('jogador-2-selecionado');
+    
+                const imagemJogador2 = document.getElementById('personagem-jogador-2');
+                imagemJogador2.src = `img/imgPersona/${idSelecionado}.png`;
+    
+                const nomejogador2 = document.getElementById('nome-jogador-2');
+                const nomeSelecionado = personagem.getAttribute('data-name');
+    
+                nomejogador2.innerHTML = nomeSelecionado;
+            })
+        })
+}
+
+export function salvarPersonagem(jogador, personagem) {
+    const jogador1 = document.getElementById("personagemJogador-1");
+    const jogador2 = document.getElementById("personagemJogador-2");
+
+    if(jogador == 'jogador1'){
+        jogador1.setAttribute("value", personagem);
+    } else if(jogador == 'jogador2'){   
+        jogador2.setAttribute("value", personagem);
+    }
+
     personagensSelecionados[jogador] = personagem;
     alert(jogador + " " + personagem)
-    document.getElementById(jogador + "Selecionado").innerText = "Selecionado";
+}
+
+export function retornaJogador1(){
+    return personagensSelecionados.jogador1;
+}
+
+export function retornaJogador2(){
+    return personagensSelecionados.jogador2;
 }
